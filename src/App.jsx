9b1696abc8e58f953f521a97873047737fe287c3
Furkan Berk Yılmazer <furkanberkyilmazer@users.noreply.github.com>
2024-05-01 21:42:51 +0300

    useEffect added

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,51 @@
+import axios from 'axios';
 import './App.css';
 import TaskCreate from './components/TaskCreate';
 import TaskList from './components/TaskList';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   const [tasks, setTasks] = useState([]);
-  const createTask = (title, taskDesc) => {
+
+
+  useEffect(async ()=>{
+      
+    const response = await axios.get("http://localhost:3000/tasks");
+    console.log("gelen data:"+response);  
+    setTasks(response.data);
+  },[])  //köşeli boş parantez sadece ilk çalıştığında çalışmasını sağlar.
+  // parantez olmazsa ilk çalıştığında ve usestateler her değiştiğinde çalışır.
+  // pazartez içerisinde bir usestate adı olursa ilk çalıştığında ve o usestate değiştiğinde çalışır.
+
+
+  const createTask =async (title, taskDesc) => {
+    const response =await axios.post("http://localhost:3000/tasks",{
+      title,taskDesc
+    })
     const createdTasks = [
       ...tasks,
-      {
-        id: Math.round(Math.random() * 999999),
-        title,
-        taskDesc,
-      },
+      // {
+      //   id: Math.round(Math.random() * 999999),
+      //   title,
+      //   taskDesc,
+      // }  //jsonserver yokken
+      response.data
     ];
     setTasks(createdTasks);
   };
 
-  const deleteTaskById = (id) => {
+  const deleteTaskById =async (id) => {
+    await axios.delete("http://localhost:3000/tasks/"+id);
     const afterDeletingTasks = tasks.filter((task) => {
       return task.id !== id;
     });
     setTasks(afterDeletingTasks);
   };
-  const editTaskById = (id, updatedTitle, updatedTaskDesc) => {
+  const editTaskById =async (id, updatedTitle, updatedTaskDesc) => {
+    await axios.put("http://localhost:3000/tasks/"+id,{
+      title:updatedTitle,
+      taskDesc:updatedTaskDesc
+    });
     const updatedTasks = tasks.map((task) => {
       if (task.id === id) {
         return { id, title: updatedTitle, taskDesc: updatedTaskDesc };
